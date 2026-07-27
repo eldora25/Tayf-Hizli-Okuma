b69d9682441f6380590d0d78f06fe3ac63454c19
eldora25 <32362177+eldora25@users.noreply.github.com>
2026-07-27 18:38:38 +0300

    Update speed_reader_engine.dart

--- lib/speed_reader_engine.dart
+++ lib/speed_reader_engine.dart
@@ -11,17 +11,11 @@ class SpeedReaderEngine {
       _words = ['Metin', 'bulunamadı!'];
       return;
     }
-    // Metni boşluklardan ve yeni satırlardan arındırarak kelimelere böler
-    _words = text
-        .replaceAll(RegExp(r'\s+'), ' ')
-        .split(' ')
-        .where((w) => w.isNotEmpty)
-        .toList();
+    _words = text.replaceAll(RegExp(r'\s+'), ' ').split(' ').where((w) => w.isNotEmpty).toList();
   }
 
   List<String> get words => _words;
 
-  /// Kelimenin uzunluğuna göre odaklanılacak harfin indeksini verir (Spritz standardı)
   static int getOptimalFocusIndex(String word) {
     int length = word.length;
     if (length <= 1) return 0;
