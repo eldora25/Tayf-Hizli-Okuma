e4273211afdb460e16f64ca60e5bee86449c0fc2
eldora25 <32362177+eldora25@users.noreply.github.com>
2026-07-28 19:26:47 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,11 @@ import 'book_database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Uygulama başlamadan önce kalıcı hafızadaki tema, font, kitap ve kalınan yerleri yükle
   await ThemeManager.instance.init();
   await BookDatabase.instance.init();
+  
   runApp(const MyApp());
 }
 
@@ -15,6 +18,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ThemeManager içindeki notifyListeners() çalıştığında arayüzü anında günceller
     return AnimatedBuilder(
       animation: ThemeManager.instance,
       builder: (context, _) {
